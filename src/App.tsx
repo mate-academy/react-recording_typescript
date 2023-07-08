a95fa9d335f263ec068b52bd1a7a496089717c09
Misha Hrynko <misha@mate.academy>
2023-07-08 13:10:32 +0300

    after Handle Typical Loading Cases

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,53 @@ import React, { useEffect, useState } from 'react';
 import { UsersList } from './components/UserList';
 import { User } from './types';
 import { getUsers } from './services/user';
+import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [updateAt, setUpdateAt] = useState(new Date());
 
   useEffect(() => {
-    getUsers().then(setUsers)
-  }, []);
+    setLoading(true);
+
+    setTimeout(() => {
+      getUsers()
+        .then(setUsers)
+        .catch(() => setErrorMessage('Try again later'))
+        .finally(() => setLoading(false));
+    }, 1000);
+  }, [updateAt]);
+
+  function reload() {
+    setUpdateAt(new Date());
+    setErrorMessage('');
+  }
 
   return (
     <div className="section py-5">
       <div className="box">
         <div className="title">Users</div>
 
-        <UsersList users={users} />
+        {loading && (
+          <Loader />
+        )}
+
+        {!loading && users.length > 0 && (
+          <UsersList users={users} />
+        )}
+
+        {!loading && !errorMessage && users.length === 0 && (
+          <p className="title is-5">There are no users</p>
+        )}
+
+        {errorMessage && (
+          <p className="notification is-danger">
+            {errorMessage}
+            <button onClick={reload}>Reload</button>
+          </p>
+        )}
       </div>
     </div>
   );
