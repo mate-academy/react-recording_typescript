db8810a257684891b4c701fa50af61e3d2983f30
Misha Hrynko <misha@mate.academy>
2023-07-09 15:24:14 +0300

    after Async Await and Parallel Requests

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,41 @@
+// #region imports
 import { User } from '../types';
 import { client } from '../utils/httpClient';
 
 export function getUsers() {
   return client.get<User[]>('/users')
     .then(users => users.slice(0, 11))
+}
+// #endregion
+
+export function getUsers2() {
+  return client.get<User[]>('/users')
+    .then(users => users.slice(0, 11))
+    .catch(error => {
+      console.log('Catch', error)
+    })
+    .finally(() => {
+      console.log('Finally');
+    })
+}
+
+export async function getUsers3() {
+  try {
+    const users = await client.get<User[]>('/users');
+
+    return users.slice(0, 11);
+  } catch (error) {
+    console.log('Catch', error)
+  } finally {
+    console.log('Finally');
+  }
+}
+
+export const getUsers4 = async() => {
+  const [users, posts] = await Promise.allSettled([
+    client.get<User[]>('/users'),
+    client.get<User[]>('/posts'),
+  ]);
+
+  return users.slice(0, 11);
 }
