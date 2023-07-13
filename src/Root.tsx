2e942aec68d077ff33bd31fe6dac39f84211ba03
Misha Hrynko <misha@mate.academy>
2023-07-13 09:05:51 +0300

    after Route Params

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { App } from './App';
+import { PostsProvider } from './store/PostsContext';
+import { UsersProvider } from './store/UsersContext';
+
 import { NewPostPage } from './pages/NewPostPage';
 import { PostDetailsPage } from './pages/PostDetailsPage';
 import { PostsPage } from './pages/PostsPage';
 import { UsersPage } from './pages/UsersPage';
 import { HomePage } from './pages/HomePage';
-import { App } from './App';
-import { PostsProvider } from './store/PostsContext';
-import { UsersProvider } from './store/UsersContext';
 
 export const Root = () => (
   <Router>
@@ -15,7 +17,9 @@ export const Root = () => (
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
-            <Route path="users" element={<UsersPage />} />
+            <Route path="users">
+              <Route path=":userId?" element={<UsersPage />} />
+            </Route>
 
             <Route path="posts">
               <Route index element={<PostsPage />} />
